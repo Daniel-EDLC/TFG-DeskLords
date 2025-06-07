abf7e38a21d840ca3290dc2c54166a28e48c5acd
DioGallardoRP <diogallardo2004@gmail.com>
2025-06-07 20:50:33 +0200

    admin css

--- src/components/MenuComponents/administracion/AdminDashboard.jsx
+++ src/components/MenuComponents/administracion/AdminDashboard.jsx
@@ -6,35 +6,36 @@ import MapsList from './map';
 import PlayersList from './player';
 import SetsList from './set';
 import GamesList from './game';
-//css
 import './admin.css';
 
 function AdminDashboard() {
   const [view, setView] = useState('cards');
 
   return (
-    <div className="admin-dashboard-wrapper">
-      <div className="admin-toolbar-fixed">
-        <h1>Panel de Administración</h1>
-        <div className="admin-buttons">
-          <button onClick={() => setView('cards')}>Cartas</button>
-          <button onClick={() => setView('abilities')}>Habilidades</button>
-          <button onClick={() => setView('decks')}>Decks</button>
-          <button onClick={() => setView('maps')}>Mapas</button>
-          <button onClick={() => setView('players')}>Jugadores</button>
-          <button onClick={() => setView('sets')}>Sets</button>
-          <button onClick={() => setView('games')}>Games</button>
+    <div className="admin-scope">
+      <div className="admin-dashboard-wrapper">
+        <div className="admin-toolbar-fixed">
+          <h1>Panel de Administración</h1>
+          <div className="admin-buttons">
+            <button onClick={() => setView('cards')}>Cartas</button>
+            <button onClick={() => setView('abilities')}>Habilidades</button>
+            <button onClick={() => setView('decks')}>Decks</button>
+            <button onClick={() => setView('maps')}>Mapas</button>
+            <button onClick={() => setView('players')}>Jugadores</button>
+            <button onClick={() => setView('sets')}>Sets</button>
+            <button onClick={() => setView('games')}>Games</button>
+          </div>
         </div>
-      </div>
 
-      <div className="admin-content-scroll">
-        {view === 'cards' && <CardsList />}
-        {view === 'abilities' && <AbilitiesList />}
-        {view === 'decks' && <DecksList />}
-        {view === 'maps' && <MapsList />}
-        {view === 'players' && <PlayersList />}
-        {view === 'sets' && <SetsList />}
-        {view === 'games' && <GamesList />}
+        <div className="admin-content-scroll">
+          {view === 'cards' && <CardsList />}
+          {view === 'abilities' && <AbilitiesList />}
+          {view === 'decks' && <DecksList />}
+          {view === 'maps' && <MapsList />}
+          {view === 'players' && <PlayersList />}
+          {view === 'sets' && <SetsList />}
+          {view === 'games' && <GamesList />}
+        </div>
       </div>
     </div>
   );
