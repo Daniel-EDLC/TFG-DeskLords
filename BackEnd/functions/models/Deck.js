e6f31fa61cf702e7674816aebb29a4cf4033c754
Daniel <danestcruz@gmail.com>
2025-06-07 17:33:13 +0200

    cambios en los modelos y en como marcar las cartas nuevas

--- BackEnd/functions/models/Deck.js
+++ BackEnd/functions/models/Deck.js
@@ -8,6 +8,7 @@ const deckSchema = new mongoose.Schema({
     image: {type: String, required: false},
     belongsTo: {type: String, enum: ['shop', 'history'] ,required: false}, // Cambiar a true cuando tenga los mazos definitivos
     price: {type: Number, required: false},
+    set: {type: String, required: false},
 })
 
 const Deck = mongoose.model('Deck', deckSchema);
